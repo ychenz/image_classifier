4aa93854bea782fb3e1b4ffe9cba90932c2a0cac
Yuchen Zhao <y376zhao@uwaterloo.ca>
2018-07-01 16:00:57 -0400

    Fixed load saved model inconsistent accuracy bug

--- fashion_mnist.py
+++ fashion_mnist.py
@@ -1,14 +1,15 @@
-from keras.datasets import fashion_mnist
-from keras.utils import np_utils
 import matplotlib.pyplot as plt
-from models import SimpleCNNModel
-import numpy as np
+from keras.datasets import fashion_mnist as fashion_mnist
+from keras.utils import np_utils
+import os
+
+from model import SimpleCNNModel
 
 class FashionData:
-    def __init__(self,lr,epochs):
+    def __init__(self,training=True):
         (X_train, y_train), (X_test, y_test) = fashion_mnist.load_data()
         self.X_train = X_train.reshape(X_train.shape[0], 28, 28, 1)
-        self.X_test = X_test.reshape(X_test.shape[0],28, 28, 1)
+        self.X_test = X_test.reshape(X_test.shape[0], 28, 28, 1)
         self.y_train = np_utils.to_categorical(y_train, 10)
         self.y_test = np_utils.to_categorical(y_test, 10)
         X_train = X_train.astype('float32')
@@ -20,21 +21,60 @@ class FashionData:
         print(self.y_train.shape)
         print(self.X_test.shape)
         print(self.y_test.shape)
-        self.model = SimpleCNNModel((28, 28, 1),10,lr=lr,epochs=epochs)
+        self.model = SimpleCNNModel((28, 28, 1), 10,training=training)
 
     def visualize(self):
         plt.imshow(self.X_train.reshape(self.X_train.shape[0], 28, 28)[0])
         plt.show()
 
-    def fit(self):
-        self.model.train(self.X_train,self.y_train,self.X_test,self.y_test)
+    def fit(self, epochs=20, lr=0.001):
+        self.model.compile(lr=lr)
+        self.model.fit(self.X_train, self.y_train, self.X_test, self.y_test, batch_size=256, epochs=epochs)
+
+    def classify(self,x,labels):
+        return self.model.classify(x,labels)
+
+def clear_models(dir):
+    for the_file in os.listdir(dir):
+        file_path = os.path.join(dir, the_file)
+        try:
+            if os.path.isfile(file_path):
+                os.unlink(file_path)
+                # elif os.path.isdir(file_path): shutil.rmtree(file_path)
+        except Exception as e:
+            print(e)
+
 
 if __name__ == '__main__':
-    # for lr in [0.01,0.001,0.0001]:
-    #     print("")
-    #     print("lr: " + str(lr))
-    #     print("")
-    #     data = FashionData(lr=lr,epochs=3)
-    #     data.fit()
-    data = FashionData(lr=0.001, epochs=20)  # 90% accuracy in 3 min
-    data.fit()
+    labels = ["T-shirt/top", "Trouser", "Pullover", "Dress", "Coat", "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"]
+
+    # Training
+    clear_models("models")
+    fashion_model = FashionData(training=True)  # 90% accuracy in 3 min
+    fashion_model.fit(epochs=10,lr=0.001)
+
+    from keras.models import Sequential,load_model
+    import keras
+
+    model = Sequential()
+    model = load_model("models/fashion_model")
+    model.load_weights("models/fashion_model.h5")  # Added this line
+    model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.SGD(),metrics=['accuracy'])
+    # default_graph = tf.get_default_graph()
+    # default_graph.finalize()
+
+    score = model.evaluate(fashion_model.X_test,fashion_model.y_test, verbose=1)
+    print('Test loss:', score[0])
+    print('Test accuracy:', score[1])
+
+    for i in range(0,len(fashion_model.y_test)):
+        x = fashion_model.X_test[i]
+        y = fashion_model.y_test[i].tolist()
+        x_new = x.reshape(1, 28, 28, 1)
+        category = model.predict_classes(x_new, verbose=1)
+
+        print("Predicted: " + labels[category[0]])
+        print("Expected: " + labels[y.index(max(y))])
+        print("")
+        plt.imshow(x.reshape(28, 28))
+        plt.show()
--- model.py
+++ model.py
@@ -1,13 +1,12 @@
 import keras
 from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
-from keras.models import Sequential
+from keras.models import Sequential,model_from_json,load_model
 import matplotlib.pyplot as plt
 
 class SimpleCNNModel:
-    def __init__(self, input_shape,num_classes, batch_size=256, epochs=10,lr=0.001):
-        self.batch_size = batch_size
-        self.epochs = epochs
-        self.history = AccuracyHistory()
+    def __init__(self, input_shape,num_classes,training=True):
+        if training:
+            self.history = AccuracyHistory()
 
         self.model = Sequential()
         self.model.add(
@@ -19,15 +18,19 @@ class SimpleCNNModel:
         self.model.add(Flatten())  # shape: 4*4*64
         print("Network output shape after flatten: "+str(self.model.output_shape))
         self.model.add(Dense(units=1024, activation='relu'))  # shape: 1024
-        self.model.add(Dropout(rate=0.4))
+        if training:
+            self.model.add(Dropout(rate=0.4))
+
         self.model.add(Dense(units=num_classes, activation='softmax'))  # shape: 10, for output comparison
-        self.model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(),
+
+    def compile(self, lr=0.001):
+        self.model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(lr=lr),
                            metrics=['accuracy'])
 
-    def train(self, x_train, y_train, x_test, y_test):
+    def fit(self,x_train, y_train, x_test, y_test,batch_size=256, epochs=10):
         self.model.fit(x_train, y_train,
-                       batch_size=self.batch_size,
-                       epochs=self.epochs,
+                       batch_size=batch_size,
+                       epochs=epochs,
                        verbose=1,
                        validation_data=(x_test, y_test),
                        callbacks=[self.history, ],
@@ -39,13 +42,38 @@ class SimpleCNNModel:
         print('Test accuracy:', score[1])
 
         # serialize model to JSON
-        model_json = self.model.to_json()
-        with open("models/fashion_model.json", "w") as json_file:
-            json_file.write(model_json)
+        # model_json = self.model.to_json()
+        # with open("models/fashion_model.json", "w") as json_file:
+        #     json_file.write(model_json)
+
         # serialize weights to HDF5
+        self.model.save("models/fashion_model")
         self.model.save_weights("models/fashion_model.h5")
         print("Saved model to disk")
         self.history.plot_acc()
+
+    def load(self,file):
+        # load weights into new model
+        # json_file = open(json_file, 'r')
+        # loaded_model_json = json_file.read()
+        # json_file.close()
+        self.model = load_model(file)
+        print("Loaded model from disk")
+
+    '''
+        :param data: 1 channel image data with shape (28,28,1)
+    '''
+    def classify(self, data,labels):
+        y = self.model.predict(data, batch_size=1, verbose=1)
+        y = y.tolist()[0]
+        print(y)
+        category = y.index(max(y))
+        return labels[category]
+
+    def evaluate(self,x_test, y_test):
+        score = self.model.evaluate(x_test, y_test, verbose=1)
+        print('Test loss:', score[0])
+        print('Test accuracy:', score[1])
 
 
 class AccuracyHistory(keras.callbacks.Callback):
